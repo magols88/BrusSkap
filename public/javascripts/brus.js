18af8fd72e07f76207020817d9656cc29facbc7d
magols88 <mago88@hotmail.no>
2024-04-06 10:25:45 +0200

    Add mobile UI

--- public/javascripts/brus.js
+++ public/javascripts/brus.js
@@ -1,19 +1,21 @@
+const form = document.getElementById("orderForm");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const quantity = document.getElementById("quantity").value;
+  fetch("/brus/order", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ quantity }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      $("#orderModal").modal("show");
+    });
+});
 
-      const form = document.getElementById("orderForm");
-      form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        const quantity = document.getElementById("quantity").value;
-        fetch("/brus/order", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ quantity }),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log(data);
-            window.location.reload();
-          });
-      });
-    
+$("#orderModal").on("hidden.bs.modal", function (e) {
+  window.location.reload();
+});
